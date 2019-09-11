2897e637f83a06cc9311e459964aa03a90db244a
max-student@outlook.com <nicholasbaker7010@gmail.com>
2019-09-11 16:38:55 -0400

    Added Methods

--- EntityFramework2SQL/Program.cs
+++ EntityFramework2SQL/Program.cs
@@ -8,14 +8,71 @@ namespace EntityFramework2SQL {
 
             using (var context = new PRSContext()) {
 
-            var vendor = context.Vendors.Contains("a");
+                //var request = new Request() {
+                //    Id = 0,
+                //    Description = "Another New Request",
+                //    Justification = "I Don't Need One...",
+                //    RejectReason = null,
+                //    DeliveryMode = "Pickup",
+                //    Status = "NEW",
+                //    Total = 0,
+                //    UserId = context.Users.SingleOrDefault(u => u.Username.Equals("testuser1")).Id
+                //};
+                //context.Request.Add(request);
+
+                //read for request(with lines) print off desc and total, then print lines associated with it (price product quantity)
+
+
+                //var product = new Products() {
+                //    Id = 0,
+                //    Name = "NewProd",
+                //    PartNbr = "45hg",
+                //    Price = 10,
+                //    Unit = "each",
+                //    PhotoPath = null,
+                //    VendorId = 2
+
+                //};
+                //context.Products.Add(product);
+
+                //var requestlinenew = new RequestLine() {
+                //    Id = 0,
+                //    RequestId = 6,
+                //    ProductId = 10,
+                //    Quantity = 2
+                //};
+                //context.RequestLine.Add(requestlinenew);
+                var dbRequest = context.Request.Find(6);
+
+                dbRequest.Total = dbRequest.RequestLine.Sum(l => l.Product.Price * l.Quantity);
+
+                Console.WriteLine($"{dbRequest.Description} {dbRequest.Status} {dbRequest.Total.ToString("C")}");
+                dbRequest.RequestLine.ToList().ForEach(r1 => {
+                    Console.WriteLine($"{r1.Product.Name,-10} {r1.Quantity,5} {r1.Product.Price.ToString("C"),10}, {(r1.Product.Price * r1.Quantity).ToString("C"),11}");
+                });
+
+                var GrandTotal = context.Request.Sum(r => r.Total);
+                    Console.WriteLine(GrandTotal);
+                
+
+                
+
+               // context.Request.Remove(dbRequest);
+                
+
+               context.SaveChanges();
+
+                var vendor = context.Vendors.Find(1);
 
             Console.WriteLine($"{vendor.Code} {vendor.Name}");
 
 
-            var users = context.Users.ToList();
+                //var users = context.Users.ToList();
+                var users = from u in context.Users.ToList()
+                            where u.Username.Contains("A") || u.Username.Contains("0")
+                            select u;
 
-            foreach(var user in users) {
+            foreach(var user in  users) {
                 Console.WriteLine($"{user.Username} {user.Firstname} {user.Lastname}");
             }
 
---
+++ EntityFramework2SQLLibrary/UsersRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace EntityFramework2SQLLibrary {
+   public class UsersRepository {
+
+        private static PRSContext context = new PRSContext();
+
+        public static List<Users> GetAll() {
+            return context.Users.ToList();
+        }
+
+        public static Users GetByPk(int id) {
+            if (id > 0) {
+                return context.Users.Find(id);
+            }
+            else throw new Exception("Must Be Greater Than 0!");
+        }
+
+        public static bool Insert(Users user) {
+            if (user == null) { throw new Exception("Can't Be NULL"); }
+            user.Id = 0;
+            context.Users.Add(user);
+
+            return context.SaveChanges() == 1;
+        }
+
+        public static bool Update(Users user) {
+            if (user == null) { throw new Exception("Can't Be NULL"); }
+            var upduser = context.Users.Find(user.Id);
+            if (user == null) { throw new Exception("No user with that Id"); }
+            upduser.Username = user.Username;
+            upduser.Password = user.Password;
+            upduser.Phone = user.Phone;
+            upduser.Firstname = user.Firstname;
+            upduser.Email = user.Email;
+            upduser.IsAdmin = user.IsAdmin;
+            upduser.IsReviewer = user.IsReviewer;
+            return context.SaveChanges() == 1;
+        }
+
+        public static bool Delete(Users user) {
+            if (user == null) { throw new Exception("Can't Be NULL"); }
+            var dbuser = context.Users.Find(user.Id);
+            if (dbuser == null) { throw new Exception("No User with Id"); }
+            context.Users.Remove(dbuser);
+            return context.SaveChanges() == 1;
+        }
+
+        public static bool Delete(int id) {
+            var user = context.Users.Find(id);
+            if(user == null) { return false; }
+            var rc = Delete(user);
+            return rc;
+        }
+
+        //Do it for Vendors
+    }
+}
